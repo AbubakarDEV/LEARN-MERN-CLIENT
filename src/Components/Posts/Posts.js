94f7fc456ae135b21521907b7b66ac673e9703fd
abubakarDev <abub1156@gmail.com>
2022-08-24 19:56:50 +0500

    added crud

---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+
+export default function Posts({ currentId, setCurrentId }) {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  return (
+    <>
+      {!posts.length ? (
+        <CircularProgress />
+      ) : (
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        >
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6} md={6}>
+              <Post post={post} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </>
+  );
+}
---
+++ src/Components/Views/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import { useDispatch } from "react-redux";
+import { getPosts } from "../actions/posts";
+
+export default function IndexView() {
+  const [currentId, setCurrentId] = useState(0);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={"/memories.png"} alt="icon" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+}
