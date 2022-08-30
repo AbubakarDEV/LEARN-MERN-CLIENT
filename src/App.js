b176895ba110f3c5332b639681f100662407b8b9
abubakarDev <abub1156@gmail.com>
2022-08-30 20:46:13 +0500

    make home page and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import IndexView from "./Components/Views/index";
 import React, { useState, useEffect } from "react";
+import Home from "./Components/Home/Home";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import SignUp from "./Components/Auth/Auth";
 
 function App() {
-  return <IndexView />;
+  return (
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="auth" element={<SignUp />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 }
 
 export default App;
